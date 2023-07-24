dd0662a7f649595790671ee17e70b566bd279744
sergio-usma <sergioandresusma@hotmail.com>
2023-07-24 16:08:01 -0500

    Add button incrementar puntuacion

--- src/components/Uno.jsx
+++ src/components/Uno.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { guardarMinombre } from '../redux/slices';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { guardarMinombre, incrementarPuntuacion } from "../redux/slices";
 
 const Uno = () => {
-    const dispatch = useDispatch();
-    const miNombreAhora = useSelector(state => state.unValor.miNombre)
-    
-    const modificar = () => {
-        dispatch(guardarMinombre('Bob Esponja'))
-    } 
+  const dispatch = useDispatch();
+  const miNombreAhora = useSelector((state) => state.unValor.miNombre);
+  const miPuntuacionAhora = useSelector((state) => state.otroValor.puntuacion);
 
-    return (
-        <>
-            <h1>{ miNombreAhora }</h1>
-            <button onClick={modificar}>Modificar mi nombre</button>
-        </>
-    )
-}
+  const modificar = () => {
+    dispatch(guardarMinombre("Bob Esponja"));
+  };
+
+  const incrementar = () => {
+    dispatch(incrementarPuntuacion());
+  };
+
+  return (
+    <>
+      <h1>{miNombreAhora}</h1>
+      <h1>{miPuntuacionAhora}</h1>
+      <button onClick={modificar}>Modificar mi nombre</button>
+      <button onClick={incrementar}>Incrementar puntuacion</button>
+    </>
+  );
+};
 
 export default Uno;
