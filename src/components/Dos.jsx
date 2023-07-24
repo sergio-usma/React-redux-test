7be71b3247a25adde9b66e8c00df9b9194d14713
sergio-usma <sergioandresusma@hotmail.com>
2023-07-24 14:44:34 -0500

    Add components basic structure

---
+++ src/components/Dos.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dos = () => {
+    return (
+        <div>
+            <h1>Dos</h1>
+        </div>
+    )
+}
+
+export default Dos;
---
+++ src/components/Uno.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Uno = () => {
+    return (
+        <div>
+            <h1>Uno</h1>
+        </div>
+    )
+}
+
+export default Uno;
